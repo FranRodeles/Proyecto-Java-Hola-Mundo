ebb5a6e83ba8b2433a5a518fbf1351cc38ed7885
Gustaf02 <92409193+Gustaf02@users.noreply.github.com>
2022-11-26 13:15:37 -0300

    Agregados Proyecto Integrador

--- ProyectoFinal_Borrador.java
+++ ProyectoFinal_Borrador.java
@@ -1,7 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+entrada: cantidad de vendedores, ventas x cada zona
+salidas: cada punto que se puede seleccionar en el menú, la matriz impresa en consola
+
+Integrantes del equipo que han trabajado en la traducción de este Proyecto Integrador desde pseint
+a Java:
+
+- Israel Leonardo Montiel 
+- Carlos Gustavo Ortiz
+
+En el código se detalla qué partes ha trabajado cada integrante. 
  */
 package proyectofinal_borrador;
 
@@ -72,8 +79,103 @@ public class ProyectoFinal_Borrador {
             }
             }
             }
-	           
+	// Subprograma: reiniciar matriz
+    // Carlos Gustavo Ortiz
         
+	public static void reiniciarmatriz(double informacion[][], double n, double m) {
+		int i;
+		int j;
+		for (i=0;i<=n-1;i++) {
+			for (j=0;j<=m-1;j++) {
+				informacion[i][j] = 0;
+			}
+		}
+	}
+    // Subprograma: verificación avance día
+    // Carlos Gustavo Ortiz
+	public static void verificacionavancedia(double informacion[][], double n, double m) {
+		int i;
+		int j;
+		for (i=0;i<=n-1;i++) {
+			for (j=0;j<=m-1;j++) {
+				if (Double.toString(m).equals(" ")) {
+					informacion[i][j] = 0;
+				}
+			}
+		}
+	}
+    // Subprograma: imprimir matriz
+    // Carlos Gustavo Ortiz
+	public static void imprimirmatriz(String matriz[][], double n, double m) {
+		int i;
+		int j;
+		System.out.println("                N S E O C");
+		for (i=0;i<=n-1;i++) {
+			System.out.print("representante "+i+1+"[");
+			for (j=0;j<=m-1;j++) {
+				System.out.print(matriz[i][j]);
+				if (j!=m-1) {
+					System.out.print(",");
+				}
+			}
+			System.out.println("]");
+		}
+	}
+    //Subprograma: copiar matriz
+    // Carlos Gustavo Ortiz
+	public static void copiarmatriz(String informacion[][], String matrizdiaanterior[][], double n, double m) {
+		int i;
+		int j;
+		for (i=0;i<=n-1;i++) {
+			for (j=0;j<=m-1;j++) {
+				matrizdiaanterior[i][j] = informacion[i][j];
+			}
+		}
+	}
+      //Subprograma Mayor venta
+// Se define qué vendedor tiene el mayor número de ventas por zona//
+//Carlos Gustavo Ortiz
+       	public static void mayorVenta(double n, double zonas, int informacion[][]) {
+		int i;
+		int j;
+		int vendedor;
+		int ventaMayor;
+		String zona="";                 //Duda si está bien inicializado
+                vendedor=1;                     // Duda si está bien inicializado
+                
+		ventaMayor = -1;
+		for (i=0;i<=n-1;i++) {
+			for (j=0;j<=zonas-1;j++) {
+				if (ventaMayor<informacion[i][j]) {
+					ventaMayor = informacion[i][j];
+					vendedor = i;
+					switch (j) {
+					case 0:
+						zona = "Norte";
+						break;
+					case 1:
+						zona = "Sur";
+						break;
+					case 2:
+						zona = "Este";
+						break;
+					case 3:
+						zona = "Oeste";
+						break;
+					case 4:
+						zona = "Centro";
+						break;
+					}
+				}
+			}
+		}
+		if (ventaMayor==0) {
+			System.out.println("No hubo ventas el dia de hoy");
+		} else {
+			System.out.print("La mayor venta fue de "+ventaMayor+" La realizó el vendedor "+vendedor+1);
+			System.out.println(" En la zona "+zona);
+		}
+	}  
         
 // Logo
 // Carlos Gustavo Ortiz
